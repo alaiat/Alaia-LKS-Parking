d2f6869efe44b50b970ae86f0c349f6c44a072b7
Alex Babel <alexander.babel@icloud.com>
2023-09-28 11:44:56 +0200

    refactor(SeeBookings): define activity operations as constants

--- app/src/main/java/com/lksnext/parkingalaiat/SeeBookings.java
+++ app/src/main/java/com/lksnext/parkingalaiat/SeeBookings.java
@@ -35,6 +35,9 @@ import java.util.List;
 
 public class SeeBookings extends AppCompatActivity {
     private static final String OPERATION = "OPERATION";
+    private static final String ADD_REQ = "ADD_REQ";
+    private static final String EDIT_REQ = "EDIT_REQ";
+
     private FirebaseManager fm;
 
     private List<Booking> bookingsToShow;
@@ -49,14 +52,15 @@ public class SeeBookings extends AppCompatActivity {
     private FloatingActionButton newBookButton;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState){
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.show_booking_view);
         initUi();
 
     }
+
     @Override
-    public boolean onCreateOptionsMenu(Menu menu){
+    public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater menuInflater = getMenuInflater();
         menuInflater.inflate(R.menu.top_bar_app, menu);
 
@@ -64,13 +68,14 @@ public class SeeBookings extends AppCompatActivity {
         return true;
     }
 
-    public void initUi(){
+    public void initUi() {
         initData();
         initView();
         initListeners();
 
     }
-    public void initData(){
+
+    public void initData() {
         allBookings = new ArrayList<>();
         bookingsToShow = new ArrayList<>();
         activeList = new ArrayList<>();
@@ -80,20 +85,24 @@ public class SeeBookings extends AppCompatActivity {
         getAllBookingsForUser();
 
         sortBookings();
-        for(Booking a : allBookings){
+        for (Booking a : allBookings) {
             checkElementStatus(a);
         }
 
 
     }
-    public void initView(){
+
+    public void initView() {
         tabs = findViewById(R.id.tabs);
         newBookButton = findViewById(R.id.nReservaButton);
         setAdapter();
 
     }
+
     private void initListeners() {
-        newBookButton.setOnClickListener(view -> { changeToNewBooking(); });
+        newBookButton.setOnClickListener(view -> {
+            changeToNewBooking();
+        });
         tabs.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
             @Override
             public void onTabSelected(TabLayout.Tab tab) {
@@ -133,22 +142,22 @@ public class SeeBookings extends AppCompatActivity {
         }).attachToRecyclerView(recyclerView);
         adapter.setOnItemClickListener(position -> {
 
-                // Handle item click event here
-                Booking r;
-                if(tabs.getSelectedTabPosition() == 0){
-                    r = activeList.get(position);
-                    fm.setCurrentBooking(r);
+            // Handle item click event here
+            Booking r;
+            if (tabs.getSelectedTabPosition() == 0) {
+                r = activeList.get(position);
+                fm.setCurrentBooking(r);
 
-                    Intent intent = new Intent(SeeBookings.this, EditBooking.class);
-                    intent.putExtra(OPERATION, "EDIT_REQ");
-                    bookingActivityLauncher.launch(intent);
-                }
+                Intent intent = new Intent(SeeBookings.this, EditBooking.class);
+                intent.putExtra(OPERATION, EDIT_REQ);
+                bookingActivityLauncher.launch(intent);
+            }
 
         });
     }
 
 
-    private void sortBookings(){
+    private void sortBookings() {
         BookingSorter rs = new BookingSorter();
         rs.sortElementsByProximity(allBookings);
         rs.sortElementsByProximity(activeList);
@@ -157,27 +166,26 @@ public class SeeBookings extends AppCompatActivity {
 
     private void changeToNewBooking() {
         Intent intent = new Intent(SeeBookings.this, NewBooking.class);
-        intent.putExtra(OPERATION, "ADD_REQ");
+        intent.putExtra(OPERATION, ADD_REQ);
         bookingActivityLauncher.launch(intent);
     }
 
 
-
-    public void changeDataToActive(){
+    public void changeDataToActive() {
         bookingsToShow.clear();
         bookingsToShow.addAll(activeList);
         adapter.notifyDataSetChanged();
     }
 
-    public void changeDataToInactive(){
+    public void changeDataToInactive() {
         bookingsToShow.clear();
         bookingsToShow.addAll(inactiveList);
         adapter.notifyDataSetChanged();
     }
 
 
-    public void setAdapter(){
-        adapter = new BookingAdapter(bookingsToShow,this);
+    public void setAdapter() {
+        adapter = new BookingAdapter(bookingsToShow, this);
         recyclerView = findViewById(R.id.listRecyclerView);
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
@@ -185,6 +193,7 @@ public class SeeBookings extends AppCompatActivity {
 
         changeDataToActive();
     }
+
     public void checkElementStatus(Booking element) {
         LocalDate currentDate = LocalDate.now();
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
@@ -194,10 +203,10 @@ public class SeeBookings extends AppCompatActivity {
             LocalTime now = LocalTime.now();
             LocalTime time = LocalTime.parse(element.getEndTime());
 
-            if(now.isAfter(time)){
+            if (now.isAfter(time)) {
                 element.setActive("Inactive");
                 inactiveList.add(element);
-            }else{
+            } else {
                 element.setActive("Active");
                 activeList.add(element);
             }
@@ -217,40 +226,40 @@ public class SeeBookings extends AppCompatActivity {
     ActivityResultLauncher<Intent> bookingActivityLauncher = registerForActivityResult(
             new ActivityResultContracts.StartActivityForResult(),
             result -> {
-                    int resultCode = result.getResultCode();
-                    Intent data = result.getData();
-                    String operation = data.getStringExtra(OPERATION);
+                int resultCode = result.getResultCode();
+                Intent data = result.getData();
+                String operation = data.getStringExtra(OPERATION);
 
-                    if (operation != null) {
-                        if (operation.equals("ADD_REQ") && resultCode == RESULT_OK) {
-                            String date = data.getStringExtra(NewBooking.EXTRA_DATE);
-                            String start = data.getStringExtra(NewBooking.EXTRA_START);
-                            String end = data.getStringExtra(NewBooking.EXTRA_END);
-                            String spot = data.getStringExtra(NewBooking.EXTRA_SPOT);
+                if (operation != null) {
+                    if (operation.equals(ADD_REQ) && resultCode == RESULT_OK) {
+                        String date = data.getStringExtra(NewBooking.EXTRA_DATE);
+                        String start = data.getStringExtra(NewBooking.EXTRA_START);
+                        String end = data.getStringExtra(NewBooking.EXTRA_END);
+                        String spot = data.getStringExtra(NewBooking.EXTRA_SPOT);
 
-                            Booking res = new Booking(spot, start, end, date, UserContext.getInstance().getCurrentUser().getUid());
-                            addBookingInFirebase(res);
-                            allBookings.add(res);
-                            checkElementStatus(res);
-                            sortBookings();
-                            changeDataToActive();
-                            adapter.notifyDataSetChanged();
+                        Booking res = new Booking(spot, start, end, date, UserContext.getInstance().getCurrentUser().getUid());
+                        addBookingInFirebase(res);
+                        allBookings.add(res);
+                        checkElementStatus(res);
+                        sortBookings();
+                        changeDataToActive();
+                        adapter.notifyDataSetChanged();
 
-                            Toast.makeText(SeeBookings.this, "Booking saved", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(SeeBookings.this, "Booking saved", Toast.LENGTH_SHORT).show();
 
-                        } else if (operation.equals("EDIT_REQ") && resultCode == RESULT_OK) {
-                            Booking newB = CurrentBooking.getInstance().getCurrent();
-                            newB.setStartTime(data.getStringExtra(NewBooking.EXTRA_START));
-                            newB.setEndTime(data.getStringExtra(NewBooking.EXTRA_END));
-                            sortBookings();
-                            changeDataToActive();
+                    } else if (operation.equals(EDIT_REQ) && resultCode == RESULT_OK) {
+                        Booking newB = CurrentBooking.getInstance().getCurrent();
+                        newB.setStartTime(data.getStringExtra(NewBooking.EXTRA_START));
+                        newB.setEndTime(data.getStringExtra(NewBooking.EXTRA_END));
+                        sortBookings();
+                        changeDataToActive();
 
-                        } else {
-                            Toast.makeText(SeeBookings.this, "Booking not saved", Toast.LENGTH_SHORT).show();
-                        }
                     } else {
-                        Toast.makeText(SeeBookings.this, "Unknown operation", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(SeeBookings.this, "Booking not saved", Toast.LENGTH_SHORT).show();
                     }
+                } else {
+                    Toast.makeText(SeeBookings.this, "Unknown operation", Toast.LENGTH_SHORT).show();
+                }
 
             }
     );
@@ -271,17 +280,19 @@ public class SeeBookings extends AppCompatActivity {
         }
 
     }
-    private void showDeleteDialog(int position){
+
+    private void showDeleteDialog(int position) {
         new MaterialAlertDialogBuilder(this)
                 .setMessage("Are you sure you want to delete the booking?")
                 .setPositiveButton("DELETE", (dialog, which) -> {
                     deleteBooking(position);
                     dialog.dismiss();
-                }).setNegativeButton("CANCEL", (dialog,which)->{
+                }).setNegativeButton("CANCEL", (dialog, which) -> {
                     dialog.dismiss();
                 })
                 .show();
     }
+
     private void deleteBookingByAllData(Booking r) {
         fm.deleteBookingByAllData(r);
     }
@@ -289,12 +300,12 @@ public class SeeBookings extends AppCompatActivity {
 
     private void deleteBooking(int position) {
         Booking r;
-        if(tabs.getSelectedTabPosition()==0){
+        if (tabs.getSelectedTabPosition() == 0) {
             r = activeList.get(position);
             activeList.remove(position);
             allBookings.remove(r);
             changeDataToActive();
-        }else{
+        } else {
             r = inactiveList.get(position);
             inactiveList.remove(position);
             allBookings.remove(r);
@@ -305,19 +316,20 @@ public class SeeBookings extends AppCompatActivity {
                 .setDuration(7000)
                 .setAction("Undo", v -> {
 
-                        addBookingInFirebase(r);
-                        allBookings.add(r);
-                        checkElementStatus(r);
-                        sortBookings();
-                        if (tabs.getSelectedTabPosition() == 0) {
-                            changeDataToActive();
-                        } else {
-                            changeDataToInactive();
-                        }
+                    addBookingInFirebase(r);
+                    allBookings.add(r);
+                    checkElementStatus(r);
+                    sortBookings();
+                    if (tabs.getSelectedTabPosition() == 0) {
+                        changeDataToActive();
+                    } else {
+                        changeDataToInactive();
+                    }
 
                 })
                 .show();
     }
+
     private void getAllBookingsForUser() {
         fm.getAllBookingsForUser(bookings -> {
             if (bookings != null) {
